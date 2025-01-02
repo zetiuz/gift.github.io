a2409ce86ff51330c25fba7aa1b9bf37527421fb
zetiuz <122922707+zetiuz@users.noreply.github.com>
2025-01-02 00:06:48 -0400

    Fix

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('.fireworks-container');
 const fireworks = new Fireworks(container, {
     hue: { min: 0, max: 360 },
-    // Aquí se añade la opción para el sonido
     sound: {
-        explosion: 'explosion.mp3', // Ruta al archivo de sonido
-        enable: true // Habilitar el sonido
+        enabled: true, // Habilitar el sonido
+        files: ['explosion.mp3'], // Ruta al archivo de sonido
+        volume: { 
+            min: 2, // Valor mínimo
+            max: 4  // Valor máximo
+        }
     },
 });
 
@@ -13,21 +16,10 @@ function startFireworks() {
     fireworks.start();
 }
 
-function playExplosionSound() {
-    const audio = new Audio('explosion.mp3'); // Crea un nuevo objeto Audio
-    audio.play(); // Reproduce el sonido
-
-    // Detener el sonido después de 2 segundos
-    setTimeout(() => {
-        audio.pause(); // Pausa el sonido
-        audio.currentTime = 0; // Reinicia el tiempo del audio
-    }, 2000); // 2000 milisegundos = 2 segundos
-}
-
 // Iniciar fuegos artificiales automáticamente al cargar la página
-window.onload = function() {
+window.onload = function () {
     startFireworks();
-    
+
     // Crear un nuevo fuego artificial cada 750 ms
     setInterval(() => {
         fireworks.start();
